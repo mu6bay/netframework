b39118b81b01260618574da3a5f32fd73d82257f
mu6bay <167513263+mu6bay@users.noreply.github.com>
2024-06-22 00:04:17 +0400

    Add files via upload

---
+++ pr1/2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToSingle(textBox1.Text);
+            double b = Convert.ToSingle(textBox2.Text);
+            double c = 0;
+            int k = comboBox1.SelectedIndex;
+            if (k == 0) c = a + b;
+            if (k == 1) c = a - b;
+            if (k == 2) c = a * b;
+            if (k == 3) c = a / b;
+            textBox3.Text = Convert.ToString(c);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ pr1/3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            radioButton1.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToSingle(textBox1.Text);
+            double b = Convert.ToSingle(textBox2.Text);
+            double c = 0;
+            if (radioButton1.Checked == true) c = a + b;
+            if (radioButton2.Checked == true) c = a - b;
+            if (radioButton3.Checked == true) c = a * b;
+            if (radioButton4.Checked == true) c = a / b;
+            textBox3.Text = Convert.ToString(c);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

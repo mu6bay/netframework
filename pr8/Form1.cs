a9cbd67687fb4008c2c7e162d59be1058ce9b3f0
mu6bay <167513263+mu6bay@users.noreply.github.com>
2024-06-22 00:10:54 +0400

    Add files via upload

---
+++ pr8/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            string a = dateTimePicker1.Value.ToLongDateString();
+            textBox1.Text = a;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = dateTimePicker1.Value.ToLongDateString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // устанавливаем новую дату
+            monthCalendar1.TodayDate = new DateTime(2016, 09, 1);
+            // убираем текущую дату снизу календаря
+            monthCalendar1.ShowToday = false;
+            // выделяем синим новую дату
+            monthCalendar1.SelectionStart = new DateTime(2016, 09, 1);
+            monthCalendar1.SelectionEnd = new DateTime(2016, 09, 1);
+            //показываем дней недели и номер недели
+            monthCalendar1.ShowWeekNumbers = true;
+
+        }
+    }
+}
